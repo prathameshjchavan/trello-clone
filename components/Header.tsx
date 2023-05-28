2c057341e7445a23172fd4e0435ac360ba8c2ba3
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-05-28 18:17:54 +0530

    header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,40 @@
-function Header() {
-	return <div>Header</div>;
-}
+"use client";
+
+import Image from "next/image";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
+import Avatar from "react-avatar";
+
+const Header = () => {
+	return (
+		<header>
+			<div className="flex flex-col md:flex-row items-center p-5 bg-gray-500/10 rounded-b-2xl">
+				<Image
+					src="https://links.papareact.com/c2cdd5"
+					alt="Trello Logo"
+					width={300}
+					height={100}
+					className="w-44 md:w-56 pb-10 md:pb-0 object-contain"
+				/>
+				<div className="flex items-center space-x-5 flex-1 justify-end w-full">
+					{/* Search Box */}
+					<form className="flex items-center space-x-5 bg-white rounded-md p-2 shadow-md flex-1 md:flex-initial">
+						<MagnifyingGlassIcon className="h-6 w-6 text-gray-400" />
+						<input
+							type="text"
+							placeholder="Search"
+							className="flex-1 outline-none p-2"
+						/>
+						<button type="submit" hidden>
+							Search
+						</button>
+					</form>
+
+					{/* Avatar */}
+					<Avatar name="Prathamesh Chavan" round color="#0055D1" size="50" />
+				</div>
+			</div>
+		</header>
+	);
+};
 
 export default Header;
