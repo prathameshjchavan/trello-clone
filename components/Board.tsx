13fd36f9f753ae2adf5eddfb66b3f9e9e6029439
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-05-29 08:54:18 +0530

    fetching appwrite data

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,18 +1,22 @@
 "use client";
+import { useBoardStore } from "@/store/boardStore";
 import { useEffect } from "react";
 import { DragDropContext, Droppable } from "react-beautiful-dnd";
 
 const Board = () => {
+	const getBoard = useBoardStore((state) => state.getBoard);
+
 	useEffect(() => {
-		// getBoard();
-	}, []);
+		getBoard();
+	}, [getBoard]);
 
 	return (
-		<DragDropContext>
-			<Droppable droppableId="board" direction="horizontal" type="column">
-				{(provided) => <div></div>}
-			</Droppable>
-		</DragDropContext>
+		<h1>Hello</h1>
+		// <DragDropContext>
+		// 	<Droppable droppableId="board" direction="horizontal" type="column">
+		// 		{(provided) => <div></div>}
+		// 	</Droppable>
+		// </DragDropContext>
 	);
 };
 
