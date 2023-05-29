4c42dfc966e6f1269a8cbce7ed18bf897cc30f72
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-05-29 09:14:04 +0530

    implementing to-do data

--- lib/getTodosGroupedByColumn.ts
+++ lib/getTodosGroupedByColumn.ts
@@ -6,5 +6,32 @@ export const getTodosGroupedByColumn = async () => {
 		process.env.NEXT_PUBLIC_TODOS_COLLECTION_ID!
 	);
 
-	console.log(data);
+	const todos = data.documents;
+
+	const columns = todos.reduce((acc, todo) => {
+		if (!acc.get(todo.status)) {
+			acc.set(todo.status, { id: todo.status, todos: [] });
+		}
+
+		acc.get(todo.status)!.todos.push({
+			$id: todo.$id,
+			$createdAt: todo.$createdAt,
+			title: todo.title,
+			status: todo.status,
+			...(todo.image && { image: JSON.parse(todo.image) }),
+		});
+
+		return acc;
+	}, new Map<TypedColumn, Column>());
+
+	// if columns doesn't have inprogress, todo and done, add them with empty todos
+	const columnTypes: TypedColumn[] = ["todo", "inprogress", "done"];
+	for (const columnType of columnTypes) {
+		if (!columns.get(columnType)) {
+			columns.set(columnType, {
+				id: columnType,
+				todos: [],
+			});
+		}
+	}
 };
