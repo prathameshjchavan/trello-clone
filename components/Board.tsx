c4308434ce2d084b6de3e9ad6883368d9d373da0
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-05-29 08:00:05 +0530

    implementing zustand

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,6 +1,12 @@
+"use client";
+import { useEffect } from "react";
 import { DragDropContext, Droppable } from "react-beautiful-dnd";
 
 const Board = () => {
+	useEffect(() => {
+		// getBoard();
+	}, []);
+
 	return (
 		<DragDropContext>
 			<Droppable droppableId="board" direction="horizontal" type="column">
---
+++ store/boardStore.ts
@@ -0,0 +1,11 @@
+import { create } from "zustand";
+
+interface BoardState {
+	board: Board;
+	getBoard: () => void;
+}
+
+const useBoardStore = create((set) => ({
+	board: null,
+	getBoard: () => {},
+}));
