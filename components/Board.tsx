093288d48310b9298cc69eada6bc3028d93b6d2e
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-05-30 12:23:16 +0530

    building column component

--- components/Board.tsx
+++ components/Board.tsx
@@ -2,6 +2,7 @@
 import { useBoardStore } from "@/store/boardStore";
 import { useEffect } from "react";
 import { DragDropContext, DropResult, Droppable } from "react-beautiful-dnd";
+import Column from "./Column";
 
 const Board = () => {
 	const { board, getBoard } = useBoardStore();
